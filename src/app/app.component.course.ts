3cefca06f236b55d855decc01df42f8962551758
Suresh Munnangi <sureshmunnangi@gmail.com>
2016-09-08 08:05:40 -0400

    commit

--- src/app/app.component.course.ts
+++ src/app/app.component.course.ts
@@ -1,15 +1,18 @@
 import {Component} from '@angular/core';
-
+import {Injectable} from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'course',
    template: `
    <h1>  This is from Course Component !!</h1>
+   <ul>
+   <li *ngFor = "let course of Courses"> {{course}} </li> 
+    </ul>
    `
 })
 export class CourseComponent{
-
+@Injectable() 
 Courses:String[] = ['English', 'French', 'Arabic'];
 
 }
